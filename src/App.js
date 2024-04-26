290070d43a1e692786df47ce70717fe6912fb525
Mary Njoki <55851306+Njokimary@users.noreply.github.com>
2024-04-26 11:56:31 +0300

    feat:create a route for the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      {/* Your other content goes here */}
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          
+          
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
