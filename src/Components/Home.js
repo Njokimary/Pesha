948989393f7ef830db8f396181ee37bc28a83cb0
Mary Njoki <55851306+Njokimary@users.noreply.github.com>
2024-04-26 11:59:18 +0300

    feat:linking the different pages of the landigpage

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './Landingpage';
+import Trusted from './Trusted';
+
+function Home() {
+  return (
+    <div>
+      <LandingPage />
+      <Trusted />
+    </div>
+  );
+}
+
+export default Home;
