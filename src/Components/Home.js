cf9cc9dcd45e974e8d5846cfa4ac3bfc6ceb99dd
Mary Njoki <55851306+Njokimary@users.noreply.github.com>
2024-04-29 09:33:44 +0300

    feat:add different pages on the landing page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LandingPage from './Landingpage';
 import Trusted from './Trusted';
+import Layout from './Layout';
 
 function Home() {
   return (
     <div>
       <LandingPage />
       <Trusted />
+      <Layout/>
     </div>
   );
 }
