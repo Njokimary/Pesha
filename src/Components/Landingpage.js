db0b6bb0099cfa0aa1446c4722bc35718ece930c
Mary Njoki <55851306+Njokimary@users.noreply.github.com>
2024-04-29 09:36:42 +0300

    feat:updates on lading page

--- src/Components/Landingpage.js
+++ src/Components/Landingpage.js
@@ -8,6 +8,7 @@ const LandingPage = () => {
         <div className="w-full md:w-1/2 px-4">
           <h1 className="text-3xl font-bold mb-4">Automated Systems Spend and Payables</h1>
           <p className="text-lg mb-4">Effectively manage your payments at any time and from any location, allowing you to organize budgets and allocate funds for a diverse range expenses with ease.</p>
+          
           <div>
             <button className="bg-[#5E52B6] text-white border border-[#5E52B6] px-4 py-2 rounded-lg mr-4">Get Started</button>
             <button className="bg-[#EFEEF8] text-[#5E52B6] border border-[#5E52B6] px-4 py-2 rounded-lg">Speak to Sales</button>
